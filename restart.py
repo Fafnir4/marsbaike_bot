447afef994757c6af8b7b529e2cf561419dcefb7
Fafnir <47449268+Fafnir4@users.noreply.github.com>
2023-01-22 00:10:09 +0600

    End

--- restart.py
+++ restart.py
@@ -1,1 +1,8 @@
-import os
+from os import system
+from time import sleep
+
+while True:
+    system('/etc/init.d/example stop')
+    sleep(10)
+    system('/etc/init.d/example start')
+    sleep(3060)
